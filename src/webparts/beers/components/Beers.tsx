4365c2e2952c1461f1fc228dd0d0bf56002708b9
Corey Roth <coreyroth@Corey-Roths-Work-MacBook-Pro.local>
2019-03-20 13:23:18 -0700

    Before changing to cards.

--- src/webparts/beers/components/Beers.tsx
+++ src/webparts/beers/components/Beers.tsx
@@ -5,7 +5,7 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import '@ionic/core/css/core.css';
 import '@ionic/core/css/ionic.bundle.css';
 
-import { IonCard, IonCardContent, IonCardHeader, IonList, IonListHeader, IonItem, IonButton } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonList, IonListHeader, IonItem, IonButton, IonThumbnail, IonLabel } from '@ionic/react';
 
 export default class Beers extends React.Component<IBeersProps, {}> {
   public render(): React.ReactElement<IBeersProps> {
@@ -15,6 +15,22 @@ export default class Beers extends React.Component<IBeersProps, {}> {
           <IonListHeader>
             Beers
           </IonListHeader>
+          {
+            this.props.beers.map(beer => (
+              <IonItem>
+                <IonThumbnail slot="start">
+                  <img src={beer.beer_label}></img>
+                </IonThumbnail>
+                <h3>
+                  {beer.beer_name}
+                </h3>
+                <br />
+                <p>
+                  {beer.brewery_name}
+                </p>
+              </IonItem>
+            ))
+          }
         </IonList>
       </div>
     );
